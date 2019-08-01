df7e73099e7a1e731d92fd3cb00ca2382950a6b3
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-08-01 15:44:47 +0200

    COMMERCE-1653 changed fe modules structure + started management toolbar

--- commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/servlet/taglib/AddressModalTag.java
+++ commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/servlet/taglib/AddressModalTag.java
@@ -62,7 +62,7 @@ public class AddressModalTag extends ComponentRendererTag {
 		}
 
 		return npmResolver.resolveModuleName(
-			"commerce-frontend-taglib/address_modal/AddressModal.es");
+			"commerce-frontend-taglib/js/address_modal/AddressModal.es");
 	}
 
 }
