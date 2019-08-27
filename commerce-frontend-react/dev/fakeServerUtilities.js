a923f600c4088ff948e90dcb83bd8edf64d839e4
Giacomo Zinetti <giacomo.zinetti@gmail.com>
2019-08-27 09:48:53 +0200

    wip gallery

--- commerce-frontend-react/dev/fakeServerUtilities.js
+++ commerce-frontend-react/dev/fakeServerUtilities.js
@@ -7,6 +7,12 @@ function defineServerResponses(app) {
 			randomId: faker.random.uuid(),
 		});
 	});
+	app.get('/api/fake-html', (_, res) => {
+		res.send(faker.fake("<script>console.log('tab content loaded')</script><p>{{name.lastName}}, {{name.firstName}} {{name.suffix}}</p><button onclick='window.sidePanel.current.toggle()'>Close</button>"));
+	});
+	app.get('/api/fake-img/:url', (req, res) => {
+		res.send(`<img src="${req.params.url}" />`);
+	});
 }
 
 module.exports = {
